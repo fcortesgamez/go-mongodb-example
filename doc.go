4edaca4ce5cb614b44568b155b62ab499ef294ac
Francisco Cortes Gamez <fcortesgamez@gmail.com>
2018-11-24 17:43:25 +0100

    Docs added

---
+++ doc.go
@@ -0,0 +1,13 @@
+// TODO: Describe clearly and concisely the package go-mongodb-example
+
+package go-mongodb-example
+
+// blank imports help docs.
+import (
+	// assert package
+	_ "github.com/stretchr/testify/assert"
+	// http package
+	_ "github.com/stretchr/testify/http"
+	// mock package
+	_ "github.com/stretchr/testify/mock"
+)
